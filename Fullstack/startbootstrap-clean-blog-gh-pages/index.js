3cc224f30e8fe93266ed9cc362c349395551402c
juzi <1277345601@qq.com>
2023-01-24 16:58:54 -0500

    ejs engine

--- Fullstack/startbootstrap-clean-blog-gh-pages/index.js
+++ Fullstack/startbootstrap-clean-blog-gh-pages/index.js
@@ -1,14 +1,31 @@
+
+
 const express =  require("express");
 const path = require("path")
 const app  = new express();
 app.use(express.static("public"));
 // app.use(express.static("path"));
+const ejs = require("ejs");
+app.set("view engine","ejs");
 app.listen(4000,()=>{
   console.log("4000 port");
 })
 
 app.get("/",(req,res)=>{
-  res.sendFile(path.resolve(__dirname, "path/index.html"));
-  
+  // res.sendFile(path.resolve(__dirname, "path/index.ejs"));
+  res.render("index");
 })
 
+app.get("/about", (req, res) => {
+  // res.sendFile(path.resolve(__dirname, "path/about.ejs"));
+  res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+  // res.sendFile(path.resolve(__dirname, "path/contact.ejs"));
+  res.render("contact");
+});
+app.get("/post", (req, res) => {
+  // res.sendFile(path.resolve(__dirname, "path/post.ejs"));
+  res.render("post");
+});
