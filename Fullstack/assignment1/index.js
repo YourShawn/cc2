657f08ce8ded214c77d7e13ba2b12bf82dc6b929
juzi <1277345601@qq.com>
2023-01-28 22:53:56 -0500

    full stack assignment1

---
+++ Fullstack/assignment1/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = new express();
+app.use(express.static("public"));
+const path = require("path");
+const ejs = require("ejs");
+app.set("view engine","ejs");
+
+app.listen(4000,()=>{
+  console.log("4000 port")
+})
+
+
+app.get("/",(req,res)=>{
+  res.render("home");
+})
+app.get("/home", (req, res) => {
+  res.render("home");
+});
+app.get("/user", (req, res) => {
+  res.render("user");
+});
