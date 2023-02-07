5f9195fd854d4059cec1aa95809f1be73fce6b0e
juzi <1277345601@qq.com>
2023-02-07 17:56:02 -0500

    week5 upload file in express

--- Fullstack/startbootstrap-clean-blog-gh-pages/index.js
+++ Fullstack/startbootstrap-clean-blog-gh-pages/index.js
@@ -2,13 +2,25 @@
 
 const express =  require("express");
 const path = require("path")
+const fileUpload = require("express-fileupload");
 const app  = new express();
 app.use(express.static("public"));
 //This is for post method pass pamaters to API
 const bodyParser = require("body-parser");
+//Parameters validation middleware. Apply the middleware
+const customerMiddleware = (req,res,next)=>{
+  console.log("I am a middleware");
+  console.log(req.files)
+  if(req.files == null || req.files.title == null){
+    return res.redirect("/posts/new");
+  }
+  next();
+}
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(fileUpload());
+//Using the middleware for this path
+app.use("/posts/store/", customerMiddleware);
 // app.use(express.static("path"));
 const ejs = require("ejs");
 
@@ -25,9 +37,11 @@ mongodb.connect(
   { useNewUrlParser: true }
 );
 
-app.get("/",(req,res)=>{
+app.get("/", async (req,res)=>{
   // res.sendFile(path.resolve(__dirname, "path/index.ejs"));
-  res.render("index");
+  const blogs = await BlogPost.find({});
+  // console.log(blogs)
+  res.render("index", { blogs });
 })
 
 app.get("/about", (req, res) => {
@@ -62,9 +76,24 @@ app.get("/posts/new",(req,res)=>{
   res.render("create");
 });
 
-app.post("/posts/store/", (req, res) => {
+app.post("/posts/store/", async (req, res, next) => {
   console.log(req.body);
-  BlogPost.create(req.body, (error, blogpost) => {
-    res.redirect("/");
-  });
+  // if(req.files == null){
+  //   redirect("/posts/new");
+  // }
+  let image = req.files.image;
+  image.mv(
+    path.resolve(__dirname, "public/assets/img", image.name),
+    async (error) => {
+      await BlogPost.create(
+        {
+          ...req.body,
+          image: "assets/img/" + image.name,
+        },
+        (error, blogpost) => {
+          res.redirect("/");
+        }
+      );
+    }
+  );
 });
--- Fullstack/startbootstrap-clean-blog-gh-pages/models/BlogPost.js
+++ Fullstack/startbootstrap-clean-blog-gh-pages/models/BlogPost.js
@@ -5,6 +5,11 @@ const Schema = mongodb.Schema;
 const BlogPostSchema = new Schema({
   title: String,
   body: String,
+  username: String,
+  datePosted:{
+    date: Date
+  },
+  image: String,
 });
 const BlogPost = mongodb.model("BlogPost", BlogPostSchema);
 module.exports = BlogPost
